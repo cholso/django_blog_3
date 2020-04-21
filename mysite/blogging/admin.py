d1994bbb3cd5819c373ace8c8cdd69db9796c1cc
Chelsea Nayan <chelseanayan@gmail.com>
2020-04-21 14:12:45 -0700

    Submitting assignment #7

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -3,7 +3,20 @@ from blogging.models import Post, Category
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    fields = ('title', 'body', 'author', 'published_date')
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+    fields = ('name', 'description')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 
